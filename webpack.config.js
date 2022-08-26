50049f2adcc724ead032f5dcd30b773823052928
FOXYSAMU <reefebghr@gmail.com>
2022-08-26 14:56:10 -0400

    Created main files

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    bundle: path.resolve(__dirname, '/src/index.js'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: `Todo List`,
+      filename: 'index.html',
+      template: 'src/main.html',
+      // favicon: './src/assets/icon.png',
+    }),
+  ],
+};
